474e692162886ec8a3b2c06d2185b0ee81b518a9
velcrots <jehwison@o.cnu.ac.kr>
2024-05-26 17:50:12 +0900

    Feature: Create radar chart
    방사형 차트 렌더링 및 스테이터스 기능을 구현하였음.
    
    방사형 차트 프레스 동작 시 평균 스테이터스 정보 확인 기능 추가

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ace/src/components/image_data.dart';
 import 'package:flutter_ace/src/controller/bottom_nav_controller.dart';
+import 'package:flutter_ace/src/pages/assessment_page.dart';
 import 'package:flutter_ace/src/pages/main_page.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/get_state_manager/src/simple/get_view.dart';
@@ -38,9 +39,7 @@ class App extends GetView<BottomNavController> {
               index: controller.pageIndex.value,
               children: [
                 MainPage(),
-                Container(
-                  child: Center(child: Text('home2')),
-                ),
+                AssessmentPage(),
                 Container(
                   child: Center(child: Text('home3')),
                 ),
